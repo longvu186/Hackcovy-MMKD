63bed7dcbc4cd2934b3f6a6e95898ed5d1460697
CaoHoangTung <caohoangtung2001@gmail.com>
2020-04-26 00:18:29 +0700

    proxyht, đang ghép frontend

--- server/index.js
+++ server/index.js
@@ -5,9 +5,23 @@ const PORT = process.env.PORT || "80";
 app.use('/auth', require('./routes/auth'));
 app.use('/person', require('./routes/person'));
 app.use('/sms', require('./routes/sms'));
+app.use(express.static('./public'));
 
 app.get("/",(req,res) => {
-    res.send("HI");
+    res.sendFile(`${__dirname}/public/homepage.html`);
+});
+
+app.get("/signin",(req,res) => {
+    res.sendFile(`${__dirname}/public/signin.html`);
+});
+
+app.get("/signup",(req,res) => {
+    res.sendFile(`${__dirname}/public/signup.html`);
+});
+
+app.get("/view-map",(req,res) => {
+    console.log(__dirname);
+    res.sendFile(`${__dirname}/public/map.html`);
 });
 
 app.listen(PORT,function(){
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../utils/data_handler');
 
 router.post('/register', (req,res) => {
     res.send('Register');
 });
 
 router.get('/login', (req,res) => {
-    res.send('Login');
+    res.send(data.makeResponse(status="success",msg="ok"));
 })
 
 module.exports = router;
