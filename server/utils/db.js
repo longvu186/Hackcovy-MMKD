4ff63bc2faeb2d74b73deb6c1506781995245e53
Alex Nguyen <alexvn.edu@gmail.com>
2020-04-25 15:46:48 -0400

    sửa file db của Hoàng Tùng để dùng mongodb

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,8 +1,20 @@
 // import { JsonDB } from 'node-json-db';
 // import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
-const JsonDB = require('node-json-db').JsonDB;
-const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
 
-const db = new JsonDB(new Config("myDataBase", true, false, '/'));
 
-module.exports = db;
+// const JsonDB = require('node-json-db').JsonDB;
+// const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+
+// const db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+// module.exports = db;
+
+// Dùng Mongo 
+const config = require('./config.json');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    User: require('../app/model/user.model.js')
+};
