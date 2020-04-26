9dc3476f91deec8219c0c5d37c8d618682bacc15
CaoHoangTung <caohoangtung2001@gmail.com>
2020-04-26 11:38:54 +0700

    handle login/register page; handle map selection

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../utils/data_handler');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 
-router.post('/register', (req,res) => {
+router.post('/register', (req, res) => {
     res.send('Register');
 });
 
-router.get('/login', (req,res) => {
-    res.send(data.makeResponse(status="success",msg="ok"));
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    const dat = db.collection('Users').findOne({ phone: req.body.phone, password: req.body.password }, function (err, result) {
+        console.log(result);
+        if (result)
+            res.redirect(`/?name=${result.phone}`);
+        else 
+            res.redirect("/signin?msg=false");
+    });
 })
 
 module.exports = router;
